400bc6034fcfe2c36033cd552b4149764a7557a8
kartik <kartikk@webelight.co.in>
2026-01-21 12:35:45 +0530

    Add ai service

--- src/shared/ai/ai.service.ts
+++ src/shared/ai/ai.service.ts
@@ -1,19 +1,22 @@
-// import { GoogleGenAI } from "@google/genai";
-// import { Injectable } from "@nestjs/common";
-// import { secretConfig } from "config/secret.config";
+import { Injectable, ServiceUnavailableException } from "@nestjs/common";
+import { GoogleGenAI } from "@google/genai";
+import { secretConfig } from "config/secret.config";
 
-// @Injectable()
-// export class AiService{
+@Injectable()
+export class AiService {
+  constructor() {}
 
-//     constructor(){
-//     }
+  async sendToGemini(prompt: string) {
+    try {
+      const ai = new GoogleGenAI({ apiKey: secretConfig.geminiApiKey });
+      const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+      });
 
-//     async sendToGemini(){
-//         const ai= new GoogleGenAI({apiKey:secretConfig.geminiApiKey})
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.0-flash",
-//     contents: prompt,
-//   });
-//     }
-
-// }
+      return response.candidates?.[0].content?.parts?.[0].text;
+    } catch {
+      throw new ServiceUnavailableException("ai response not recieved");
+    }
+  }
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from "@nestjs/common";
+import { AiService } from "./ai/ai.service";
 import { CloudinaryService } from "./cloudinary/cloudinary.service";
 
 @Global()
 @Module({
-  providers: [CloudinaryService],
-  exports: [CloudinaryService],
+  providers: [CloudinaryService, AiService],
+  exports: [CloudinaryService, AiService],
 })
 export class SharedModule {}
